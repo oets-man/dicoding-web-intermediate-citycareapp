96d25fe00a312fc4e108af4bdb19e1b377d84bf2
oets <oets.man@gmail.com>
2025-05-11 10:27:45 +0700

    idb

--- src/scripts/pages/report-detail/report-detail-presenter.js
+++ src/scripts/pages/report-detail/report-detail-presenter.js
@@ -4,11 +4,13 @@ export default class ReportDetailPresenter {
   #reportId;
   #view;
   #apiModel;
+  #dbModel;
 
-  constructor(reportId, { view, apiModel }) {
+  constructor(reportId, { view, apiModel, dbModel }) {
     this.#reportId = reportId;
     this.#view = view;
     this.#apiModel = apiModel;
+    this.#dbModel = dbModel;
   }
 
   async showReportDetailMap() {
@@ -113,6 +115,27 @@ export default class ReportDetailPresenter {
     }
   }
 
+  async saveReport() {
+    try {
+      const report = await this.#apiModel.getReportById(this.#reportId);
+      await this.#dbModel.putReport(report.data);
+      this.#view.saveToBookmarkSuccessfully('Success to save to bookmark');
+    } catch (error) {
+      console.error('saveReport: error:', error);
+      this.#view.saveToBookmarkFailed(error.message);
+    }
+  }
+
+  async removeReport() {
+    try {
+      await this.#dbModel.removeReport(this.#reportId);
+      this.#view.removeFromBookmarkSuccessfully('Success to remove from bookmark');
+    } catch (error) {
+      console.error('removeReport: error:', error);
+      this.#view.removeFromBookmarkFailed(error.message);
+    }
+  }
+
   showSaveButton() {
     if (this.#isReportSaved()) {
       this.#view.renderRemoveButton();
@@ -122,7 +145,14 @@ export default class ReportDetailPresenter {
     this.#view.renderSaveButton();
   }
 
-  #isReportSaved() {
-    return false;
+  async showSaveButton() {
+    if (await this.#isReportSaved()) {
+      this.#view.renderRemoveButton();
+      return;
+    }
+    this.#view.renderSaveButton();
+  }
+  async #isReportSaved() {
+    return !!(await this.#dbModel.getReportById(this.#reportId));
   }
 }
